8e083a880675c66976db589a61db48183c2e68e3
TBO <tbo@MACs-MacBook-Pro.local>
2026-01-21 08:42:55 +0100

    chore: update deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ export PATH=$PWD/node-v20.10.0-darwin-arm64/bin:$PATH
 echo "🚀 Starting Vercel Deployment..."
 echo "Verify that you are logged in (browser will open if needed)."
 
-# Run Vercel
-vercel
+# Run Vercel (Prod)
+vercel --token 9hWbmrcaJw0WZuDmzkJIfEaz --prod
